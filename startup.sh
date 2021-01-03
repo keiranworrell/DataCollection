9d71fee45506ebb6125214a4c91ab3845991d6ee
keiranworrell <worrellkeiran@gmail.com>
2021-01-03 11:35:24 +0000

    Setup postgres deployment

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Start the postgres pod
-kubectl apply -f ./postgres/db.yaml
+kubectl apply -f ./postgres/postgres-configmap.yaml
+kubectl apply -f ./postgres/postgres-storage.yam
+kubectl apply -f ./postgres/postgres-deployment.yaml
