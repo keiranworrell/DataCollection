7c0d6d4abce7905862756c7243b6ea8c84853735
keiranworrell <worrellkeiran@gmail.com>
2021-01-03 11:47:35 +0000

    Add postgres service

--- startup.sh
+++ startup.sh
@@ -4,3 +4,4 @@
 kubectl apply -f ./postgres/postgres-configmap.yaml
 kubectl apply -f ./postgres/postgres-storage.yaml
 kubectl apply -f ./postgres/postgres-deployment.yaml
+kubectl apply -f ./postgres/postgres-service.yaml
