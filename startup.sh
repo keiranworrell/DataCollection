f311064938ba2ccde8211f2a02961ce5551716a4
keiranworrell <worrellkeiran@gmail.com>
2021-01-03 11:36:35 +0000

    typo fix

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 
 # Start the postgres pod
 kubectl apply -f ./postgres/postgres-configmap.yaml
-kubectl apply -f ./postgres/postgres-storage.yam
+kubectl apply -f ./postgres/postgres-storage.yaml
 kubectl apply -f ./postgres/postgres-deployment.yaml
