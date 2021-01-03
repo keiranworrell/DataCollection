0121f7680f49ee46110fa1b6892c82c43c330c21
keiranworrell <worrellkeiran@gmail.com>
2021-01-03 11:59:40 +0000

    Introduce apply and delete actions

--- startup.sh
+++ startup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+action=$1
 
-# Start the postgres pod
-kubectl apply -f ./postgres/postgres-configmap.yaml
-kubectl apply -f ./postgres/postgres-storage.yaml
-kubectl apply -f ./postgres/postgres-deployment.yaml
-kubectl apply -f ./postgres/postgres-service.yaml
+if [ "${action}" = "apply" ]; then
+    # Start the postgres deployment
+    kubectl apply -f ./postgres/postgres-configmap.yaml
+    kubectl apply -f ./postgres/postgres-storage.yaml
+    kubectl apply -f ./postgres/postgres-deployment.yaml
+    kubectl apply -f ./postgres/postgres-service.yaml
+elif [ "${action}" = "delete" ]; then 
+    # Delete the postgres deployment
+    kubectl delete -f ./postgres/postgres-deployment.yaml
+    kubectl delete -f ./postgres/postgres-configmap.yaml
+    kubectl delete -f ./postgres/postgres-storage.yaml
+    kubectl delete -f ./postgres/postgres-service.yaml
+else
+    echo "Please supply a valid action: apply/delete"
+fi
