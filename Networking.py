c671ef2c90ed8cfd46520be7907acb1e2029cfac
keiranworrell <worrellkeiran@gmail.com>
2021-02-10 14:29:19 +0000

    Introduce page numbers for interactions

--- Networking.py
+++ Networking.py
@@ -6,9 +6,7 @@ from os import path
 
 
 def unrecognisedCharacterPopUp(char):
-    print(
-        "ERROR: Cannot record interaction for unrecognised character {}".format(char1)
-    )
+    print("ERROR: Cannot record interaction for unrecognised character {}".format(char))
     win = tk.Toplevel()
     win.wm_title("Unrecognised Character: " + char)
 
@@ -32,9 +30,10 @@ def getCharacterID(char):
     return None
 
 
-def createLink(c1, c2, type):
+def createLink(c1, c2, type, pn):
     char1 = c1.get()
     char2 = c2.get()
+    page = pn.get()
     if char1 == char2:
         print("ERROR: Cannot record interaction between a character and themself.")
         return
@@ -56,12 +55,17 @@ def createLink(c1, c2, type):
             else:
                 edge[3] += 1
             linkAlreadyExists = True
+            edge[5] = edge[5] + ", {}".format(page)
             break
     if not linkAlreadyExists:
-        edges.append([char1ID, char2ID, "undirected", 1, type])
+        edges.append([char1ID, char2ID, "undirected", 1, type, str(page)])
         if type == "hostile":
             edges[-1][3] = -1
-    print("LOG: Interaction between {} and {} logged".format(char1, char2))
+    print(
+        "LOG: Interaction between {} and {} logged on page {}".format(
+            char1, char2, page
+        )
+    )
 
 
 def newNode(char, pn, gen):
@@ -93,6 +97,8 @@ function = int(input("Data Collection (1) or Network Analysis (2)?\n"))
 nodes_path = input('Path to nodes.csv (enclose path in " marks) : ')
 edges_path = input('Path to edges.csv (enclose path in " marks) : ')
 
+
+# If csv files exist, read data. Else, set edges and nodes
 if path.exists(nodes_path):
     with open(nodes_path, "r") as f:
         reader = csv.reader(f)
@@ -105,13 +111,13 @@ if path.exists(edges_path):
         reader = csv.reader(f)
         edges = list(reader)
 else:
-    edges = [["Source", "Target", "Type", "Weight", "Friendly/Hostile"]]
+    edges = [["Source", "Target", "Type", "Weight", "Friendly/Hostile", "Pages"]]
 
 # If csv files exist but are empty, populate with headings
 if len(nodes) < 1:
     nodes = [["ID", "Name", "Page introduced", "Gender"]]
 if len(edges) < 1:
-    edges = [["Source", "Target", "Type", "Weight", "Friendly/Hostile"]]
+    edges = [["Source", "Target", "Type", "Weight", "Friendly/Hostile", "Pages"]]
 
 if function == 1:
     # Start GUI to collect data and add nodes/edges to lists
@@ -124,30 +130,36 @@ if function == 1:
     tab1 = ttk.Frame(tabControl)
     tab2 = ttk.Frame(tabControl)
 
-    tabControl.add(tab1, text="Record interaction")
-    tabControl.add(tab2, text="Record character")
+    tabControl.add(tab1, text="Record character")
+    tabControl.add(tab2, text="Record interaction")
 
     # Populate interaction tab
     char1 = tk.StringVar()
     char2 = tk.StringVar()
+    pageNum = tk.StringVar()
 
-    ttk.Label(tab1, text="Character 1").grid(column=0, row=0, padx=30, pady=10)
-    c1 = ttk.Entry(tab1, textvariable=char1).grid(column=0, row=1, padx=30, pady=10)
+    ttk.Label(tab2, text="Character 1").grid(column=0, row=0, padx=30, pady=10)
+    c1 = ttk.Entry(tab2, textvariable=char1).grid(column=0, row=1, padx=30, pady=10)
 
-    ttk.Label(tab1, text="Character 2").grid(column=2, row=0, padx=30, pady=10)
-    c2 = ttk.Entry(tab1, textvariable=char2).grid(column=2, row=1, padx=30, pady=10)
+    ttk.Label(tab2, text="Character 2").grid(column=1, row=0, padx=30, pady=10)
+    c2 = ttk.Entry(tab2, textvariable=char2).grid(column=1, row=1, padx=30, pady=10)
 
-    callFriendly = partial(createLink, char1, char2, "friendly")
-    callHostile = partial(createLink, char1, char2, "hostile")
-    friendly = ttk.Button(tab1, text="Create friendly link", command=callFriendly).grid(
-        column=0, row=2, padx=30, pady=20
+    ttk.Label(tab2, text="Interaction on page: ").grid(
+        column=0, row=2, padx=30, pady=10
     )
-    hostile = ttk.Button(tab1, text="Create hostile link", command=callHostile).grid(
-        column=2, row=2, padx=30, pady=20
+    pn = ttk.Entry(tab2, textvariable=pageNum).grid(column=1, row=2, padx=30, pady=10)
+
+    callFriendly = partial(createLink, char1, char2, "Friendly", pageNum)
+    callHostile = partial(createLink, char1, char2, "Hostile", pageNum)
+    friendly = ttk.Button(tab2, text="Create friendly link", command=callFriendly).grid(
+        column=0, row=3, padx=30, pady=20
+    )
+    hostile = ttk.Button(tab2, text="Create hostile link", command=callHostile).grid(
+        column=1, row=3, padx=30, pady=20
     )
 
-    save = ttk.Button(tab1, text="Save CSV file", command=saveFile).grid(
-        column=1, row=3, padx=10, pady=10
+    save = ttk.Button(tab2, text="Save CSV file", command=saveFile).grid(
+        column=0, row=4, padx=10, pady=10
     )
 
     # Populate character tab
@@ -155,31 +167,35 @@ if function == 1:
     page = tk.StringVar()
     gender = tk.StringVar()
 
-    ttk.Label(tab2, text="Character Name: ").grid(column=0, row=0, padx=10, pady=10)
-    charName = ttk.Entry(tab2, textvariable=name).grid(
+    ttk.Label(tab1, text="Character Name: ").grid(column=0, row=0, padx=10, pady=10)
+    charName = ttk.Entry(tab1, textvariable=name).grid(
         column=2, row=0, padx=10, pady=10
     )
 
-    ttk.Label(tab2, text="Page first appear on: ").grid(
+    ttk.Label(tab1, text="Page first appear on: ").grid(
         column=0, row=1, padx=10, pady=10
     )
-    charName = ttk.Entry(tab2, textvariable=page).grid(
+    charName = ttk.Entry(tab1, textvariable=page).grid(
         column=2, row=1, padx=10, pady=10
     )
 
-    M = ttk.Radiobutton(tab2, text="Male", variable=gender, value="M").grid(
+    M = ttk.Radiobutton(tab1, text="Male", variable=gender, value="M").grid(
         column=0, row=2, padx=20, pady=20
     )
-    F = ttk.Radiobutton(tab2, text="Female", variable=gender, value="F").grid(
+    F = ttk.Radiobutton(tab1, text="Female", variable=gender, value="F").grid(
         column=1, row=2, padx=20, pady=20
     )
-    unknown = ttk.Radiobutton(tab2, text="Unknown", variable=gender, value="").grid(
+    unknown = ttk.Radiobutton(tab1, text="Unknown", variable=gender, value="").grid(
         column=2, row=2, padx=20, pady=20
     )
 
     callNewNode = partial(newNode, name, page, gender)
-    add = ttk.Button(tab2, text="Add character", command=callNewNode).grid(
-        column=1, row=3, padx=20, pady=20
+    add = ttk.Button(tab1, text="Add character", command=callNewNode).grid(
+        column=0, row=3, padx=20, pady=20
+    )
+
+    save = ttk.Button(tab1, text="Save CSV file", command=saveFile).grid(
+        column=2, row=3, padx=20, pady=20
     )
 
     tabControl.pack(expand=1, fill="both")
