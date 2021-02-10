54cfad3ad1f4263302fe472ce6bca37179db1d78
keiranworrell <worrellkeiran@gmail.com>
2021-02-10 11:54:26 +0000

    Refactoring

--- Networking.py
+++ Networking.py
@@ -5,77 +5,85 @@ from functools import partial
 from os import path
 
 
-def newCharPopUp(char):
+def unrecognisedCharacterPopUp(char):
     win = tk.Toplevel()
     win.wm_title("Unrecognised Character: " + char)
-    name = tk.StringVar()
-    page = tk.StringVar()
-    gender = tk.StringVar()
 
-    ttk.Label(win, text="Character Name: ").grid(column=0, row=0, padx=10, pady=10)
-    charName = ttk.Entry(win, textvariable=name).grid(column=2, row=0, padx=10, pady=10)
-
-    ttk.Label(win, text="Page first appear on: ").grid(
-        column=0, row=1, padx=10, pady=10
+    ttk.Label(win, text="The character {} was not recognised.".format(char)).grid(
+        column=0, row=0, padx=10, pady=10
     )
-    charName = ttk.Entry(win, textvariable=page).grid(column=2, row=1, padx=10, pady=10)
+    ttk.Label(
+        win,
+        text="Please record this character before including them in any interactions.",
+    ).grid(column=0, row=1, padx=10, pady=10)
 
-    M = ttk.Radiobutton(win, text="Male", variable=gender, value="M").grid(
+    add = ttk.Button(win, text="OK", command=win.destroy).grid(
         column=0, row=2, padx=20, pady=20
     )
-    F = ttk.Radiobutton(win, text="Female", variable=gender, value="F").grid(
-        column=1, row=2, padx=20, pady=20
-    )
-    unknown = ttk.Radiobutton(win, text="Unknown", variable=gender, value="").grid(
-        column=2, row=2, padx=20, pady=20
-    )
 
-    callNewNode = partial(newNode, name, page, gender)
-    add = ttk.Button(win, text="Add character", command=callNewNode).grid(
-        column=1, row=3, padx=20, pady=20
-    )
+
+def getCharacterID(char):
+    for node in nodes:
+        if char == node[1]:
+            return node[0]
+    return None
 
 
 def createLink(c1, c2, type):
     char1 = c1.get()
     char2 = c2.get()
-    linkAdded = False
+    if char1 == char2:
+        print("ERROR: Cannot record interaction between a character and themself.")
+        return
+    char1ID = getCharacterID(char1)
+    char2ID = getCharacterID(char2)
+    if char1ID == None:
+        print(
+            "ERROR: Cannot record interaction for unrecognised character {}".format(
+                char1
+            )
+        )
+        unrecognisedCharacterPopUp(char1)
+    if char2ID == None:
+        print(
+            "ERROR: Cannot record interaction for unrecognised character {}".format(
+                char2
+            )
+        )
+        unrecognisedCharacterPopUp(char2)
+    if char1ID == None or char2ID == None:
+        return
+    linkAlreadyExists = False
     for edge in edges:
-        if ((edge[0] == char1) and (edge[1] == char2) and (edge[4] == type)) or (
-            (edge[1] == char1) and (edge[0] == char2) and (edge[4] == type)
+        if (edge[0] == char1ID and edge[1] == char2ID and edge[4] == type) or (
+            edge[1] == char1ID and edge[0] == char2ID and edge[4] == type
         ):
-            edge[3] += 1
-            linkAdded = True
+            if type == "hostile":
+                edge[3] -= 1
+            else:
+                edge[3] += 1
+            linkAlreadyExists = True
             break
-    if not linkAdded:
-        edges.append([char1, char2, "undirected", 1, type])
+    if not linkAlreadyExists:
+        edges.append([char1ID, char2ID, "undirected", 1, type])
         if type == "hostile":
             edges[-1][3] = -1
-    if not characterExists(char1):
-        newCharPopUp(char1)
-    if not characterExists(char2):
-        newCharPopUp(char2)
-
-
-def characterExists(char):
-    for node in nodes:
-        if node[1] == char:
-            return True
-        else:
-            return False
+    print("LOG: Interaction between {} and {} logged".format(char1, char2))
 
 
 def newNode(char, pn, gen):
     character = char.get()
-    pageNum = pn.get()
-    gender = gen.get()
-    existingChar = False
-    if not characterExists(char):
+    if getCharacterID(character) == None:
+        pageNum = pn.get()
+        gender = gen.get()
         if len(nodes) == 1:
             id = 1
         else:
             id = int(nodes[-1][0]) + 1
         nodes.append([id, character, pageNum, gender])
+        print("LOG: {} added to nodes.csv".format(character))
+    else:
+        print("ERROR: {} already present in nodes.csv".format(character))
 
 
 def saveFile():
